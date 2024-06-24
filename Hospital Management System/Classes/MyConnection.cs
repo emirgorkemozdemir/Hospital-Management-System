cb3f449bdc356fda2cd39d8c8213e69639c1a5b1
Emir Görkem Özdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2024-06-24 21:47:37 +0300

    Add project files.

---
+++ Hospital Management System/Classes/MyConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hospital_Management_System.Classes
+{
+    internal class MyConnection
+    {
+        public static SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=HospitalManagement;Integrated Security=True;TrustServerCertificate=True");
+        
+        public static void CheckConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed) 
+            {
+                connection.Open();
+            }
+        }
+    }
+}
